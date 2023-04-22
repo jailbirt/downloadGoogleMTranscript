13d582651aed0f8522c613913560c6729dcacb53
jailbirt <jailbirt@gmail.com>
2023-04-22 02:10:36 -0300

    Este funciona generando

--- content.js
+++ content.js
@@ -1,20 +1,35 @@
-const outputFile = "captions_output.txt";
+let captionsData = "";
+let captureActive = false;
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === "capture") {
-    console.log("Capture message received");
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === "startCapture") {
+    console.log("Start capture message received");
+    captureActive = true;
     captureCaptions();
+    sendResponse({ success: true });
+  } else if (message.action === "stopCapture") {
+    console.log("Stop capture message received");
+    captureActive = false;
+    createDownloadLink(captionsData);
+    sendResponse({ success: true });
+    captionsData = "";
   }
 });
 
 async function captureCaptions() {
-  while (true) {
+  while (captureActive) {
     await new Promise((resolve) => setTimeout(resolve, 5000));
     const captionsElement = document.querySelector("div[jsname='YSxPC']");
     if (captionsElement) {
       const captions = captionsElement.innerText;
       console.log("Captured captions:", captions);
+      captionsData += captions + "\n";
     }
   }
 }
 
+function createDownloadLink(data) {
+  const blob = new Blob([data], { type: "text/plain;charset=utf-8" });
+  const url = URL.createObjectURL(blob);
+  chrome.runtime.sendMessage({ action: "downloadCaptions", url });
+}
--- popup.js
+++ popup.js
@@ -1,6 +1,25 @@
-document.getElementById("capture-button").addEventListener("click", () => {
+document.getElementById("start-capture-button").addEventListener("click", () => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.tabs.sendMessage(tabs[0].id, { action: "capture" });
-    console.log("Capture message sent");
+    chrome.tabs.sendMessage(tabs[0].id, { action: "startCapture" }, (response) => {
+      console.log("Start capture message sent", response);
+    });
   });
+});
+
+document.getElementById("stop-capture-button").addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, { action: "stopCapture" }, (response) => {
+      console.log("Stop capture message sent", response);
+    });
+  });
+});
+
+chrome.runtime.onMessage.addListener((message) => {
+  if (message.action === "downloadCaptions") {
+    const link = document.createElement("a");
+    link.href = message.url;
+    link.download = "captions.txt";
+    link.textContent = "Download captions";
+    document.body.appendChild(link);
+  }
 });
