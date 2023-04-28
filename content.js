3b1829284a27ee1f3475bf3b05ec275a4998ff0b
jailbirt <jailbirt@gmail.com>
2023-04-27 23:55:21 -0300

    Improved text reduction

--- content.js
+++ content.js
@@ -1,5 +1,6 @@
 let captionsData = "";
 let captureActive = false;
+const debug = 1; // Set to 1 for enabling debug, and 0 for disabling it
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === "startCapture") {
@@ -8,12 +9,12 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     captureCaptions();
     sendResponse({ success: true });
   } else if (message.action === "stopCapture") {
-    console.log("Stop capture message received");
-    captureActive = false;
-    const processedCaptions = processCaptions(captionsData);
-    createDownloadLink(processedCaptions);
-    sendResponse({ success: true });
-    captionsData = "";
+  console.log("Stop capture message received");
+  captureActive = false;
+  const processedData = processCaptions(captionsData);
+  createDownloadLink(captionsData, processedData);
+  sendResponse({ success: true });
+  captionsData = "";
   }
 });
 
@@ -33,13 +34,15 @@ function processCaptions(captions) {
   const lines = captions.split('\n');
   const filteredLines = [];
 
+  const removePunctuation = (str) => str.replace(/[\p{P}\p{Z}]/gu, "");
+
   for (let i = 0; i < lines.length; i++) {
     let isSubsequence = false;
-    for (let j = i + 1; j < lines.length && j < i + 10; j++) {
-      const currentLine = lines[i].slice(0, -1).toLowerCase();
-      const nextLine = lines[j].slice(0, -1).toLowerCase();
+    for (let j = i + 1; j < lines.length && j < i + 50; j++) {
+      const currentLine = removePunctuation(lines[i].toLowerCase());
+      const nextLine = removePunctuation(lines[j].toLowerCase());
 
-      if (nextLine.length > currentLine.length && nextLine.startsWith(currentLine)) {
+      if (nextLine.length > currentLine.length && nextLine.includes(currentLine)) {
         isSubsequence = true;
         break;
       }
@@ -54,8 +57,19 @@ function processCaptions(captions) {
 }
 
 
-function createDownloadLink(data) {
+
+function createDownloadLink(originalData, processedData) {
+  let data = "";
+  if (debug) {
+    data += "****************************************************** DEBUG ENABLED ************************\n";
+    data += "****************************************************** ORIGINAL ************************\n";
+    data += originalData;
+    data += "\n\n****************************************************** PROCESSED *******************\n";
+  }
+  data += processedData;
+
   const blob = new Blob([data], { type: "text/plain;charset=utf-8" });
   const url = URL.createObjectURL(blob);
   chrome.runtime.sendMessage({ action: "downloadCaptions", url });
 }
+
